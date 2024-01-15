2154c59a59ebabeb17048820f28e4a2bf27677d1
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-01-15 10:30:15 +0100

    create Header component

---
+++ components/Header/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  background-color: lightgrey;
+`;
+
+export default function Header() {
+  return <Title> Placeholder DIY-APP </Title>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import { initialProjects } from "@/lib/data";
+import Header from "@/components/Header";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Header />
     </div>
   );
 }
