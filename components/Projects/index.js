ac6d9aa8f8283f14074f1d0920d44a8bbe317ee5
Steffen Anslinger <steffen.anslinger@hotmail.com>
2024-01-15 14:40:56 +0100

    update screenview

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -2,12 +2,16 @@ import { initialProjects } from "@/lib/data";
 import styled from "styled-components";
 import Image from "next/image";
 
+const StyledSection = styled.section`
+  margin: 0 auto;
+  max-width: 650px;
+`;
+
 const ProjectCard = styled.li`
   background-color: lightgray;
   display: flex;
   flex-direction: column;
   flex: 0 0 calc(50% - 1rem);
-  max-width: calc(50% - 1rem);
   box-sizing: border-box;
   border-radius: 0.5rem;
   padding: 1rem;
@@ -26,19 +30,21 @@ const StyledList = styled.ul`
 
 export default function Projects() {
   return (
-    <StyledList>
-      {initialProjects.map((project) => (
-        <ProjectCard key={project.slug}>
-          {" "}
-          <Image
-            src={project.image}
-            width={150}
-            height={150}
-            alt={project.title}
-          />{" "}
-          {project.title}
-        </ProjectCard>
-      ))}
-    </StyledList>
+    <StyledSection>
+      <StyledList>
+        {initialProjects.map((project) => (
+          <ProjectCard key={project.slug}>
+            {" "}
+            <Image
+              src={project.image}
+              width={150}
+              height={150}
+              alt={project.title}
+            />{" "}
+            {project.title}
+          </ProjectCard>
+        ))}
+      </StyledList>
+    </StyledSection>
   );
 }
