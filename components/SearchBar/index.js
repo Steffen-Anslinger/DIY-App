1854f3d524e08e8da3325e2bd916cd97a037bf8f
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-01-19 11:55:52 +0100

    add fuzzy search function

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,18 +1,46 @@
+import React, { useState, useEffect } from "react";
 import Fuse from "fuse.js";
 import styled from "styled-components";
+import Projects from "@/components/Projects"; // Import your Projects component
 
 const StyledSearchBar = styled.div`
   padding-top: 20px;
 `;
 
-export default function SearchBar(projects) {
-  const fuse = new Fuse(projects, {
-    keys: ["title", "slug"],
-  });
+const SearchBar = ({ projects }) => {
+  const [searchPattern, setSearchPattern] = useState("");
+  const [filteredProjects, setFilteredProjects] = useState([]);
+
+  useEffect(() => {
+    const fuseOptions = {
+      keys: ["title"],
+    };
+    const fuse = new Fuse(projects, fuseOptions);
+    if (searchPattern.trim() === "") {
+      setFilteredProjects(projects);
+    } else {
+      const results = fuse.search(searchPattern);
+      const filtered = results.map((result) => result.item);
+      setFilteredProjects(filtered);
+    }
+  }, [searchPattern, projects]);
+
+  const handleSearchChange = (event) => {
+    setSearchPattern(event.target.value);
+  };
 
   return (
     <StyledSearchBar>
-      <input type="search" placeholder="Search"></input>
+      <input
+        type="search"
+        placeholder="Search"
+        value={searchPattern}
+        onChange={handleSearchChange}
+      />
+
+      <Projects projects={searchPattern ? filteredProjects : projects} />
     </StyledSearchBar>
   );
-}
+};
+
+export default SearchBar;
