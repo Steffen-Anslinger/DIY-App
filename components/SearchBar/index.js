776409e62a5649c572c059525e6380c63b10a050
Steffen Anslinger <steffen.anslinger@hotmail.com>
2024-01-23 15:23:28 +0100

    update searchbar code to render projects coorectly

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -7,33 +7,14 @@ const StyledSearchBar = styled.div`
   padding-top: 20px;
 `;
 
-export default function SearchBar({ projects, favourites, onToggleFavourite }) {
-  const [searchPattern, setSearchPattern] = useState("");
-
-  const fuseOptions = {
-    keys: ["title"],
-  };
-  const fuse = new Fuse(projects, fuseOptions);
-  const filteredProjects = fuse
-    .search(searchPattern)
-    .map((result) => result.item);
-
-  const handleSearchChange = (event) => {
-    setSearchPattern(event.target.value);
-  };
-
+export default function SearchBar({ searchPattern, onChange }) {
   return (
     <StyledSearchBar>
       <input
         type="search"
         placeholder="Search"
         value={searchPattern}
-        onChange={handleSearchChange}
-      />
-      <Projects
-        projects={searchPattern ? filteredProjects : projects}
-        favourites={favourites}
-        onToggleFavourite={onToggleFavourite}
+        onChange={onChange}
       />
     </StyledSearchBar>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,37 @@ import Projects from "@/components/Projects";
 import Navigation from "@/components/Navigation";
 import StyledSection from "@/components/Layout/StyledSection";
 import SearchBar from "@/components/SearchBar";
+import Fuse from "fuse.js";
+import React, { useState } from "react";
 
 export default function HomePage({ projects, favourites, onToggleFavourite }) {
+  const [searchPattern, setSearchPattern] = useState("");
+
+  const fuseOptions = {
+    keys: ["title"],
+  };
+  const fuse = new Fuse(projects, fuseOptions);
+  const filteredProjects = fuse
+    .search(searchPattern)
+    .map((result) => result.item);
+
+  const handleSearchChange = (event) => {
+    setSearchPattern(event.target.value);
+  };
+
   return (
     <>
       <Header />
       <StyledSection>
         <SearchBar
+          searchPattern={searchPattern}
+          onChange={handleSearchChange}
           projects={projects}
+          favourites={favourites}
+          onToggleFavourite={onToggleFavourite}
+        />
+        <Projects
+          projects={searchPattern ? filteredProjects : projects}
           favourites={favourites}
           onToggleFavourite={onToggleFavourite}
         />
