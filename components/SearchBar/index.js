cd80f8e1f9a823d03b691d3256f95d162c5c2d02
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-01-25 09:01:17 +0100

    search bar styling

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -2,13 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledSearchBar = styled.div`
-  padding-top: 20px;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 8px;
+  margin-left: calc(8px);
+  margin-right: calc(8px);
+  margin-top: calc(15px);
+`;
+
+const StyledSearchInput = styled.input`
+  border: none;
+  outline: none;
+  padding: 8px;
+  font-size: 16px;
+  width: 200px;
 `;
 
 export default function SearchBar({ searchPattern, onChange }) {
   return (
     <StyledSearchBar>
-      <input
+      <StyledSearchInput
         type="search"
         placeholder="Search"
         value={searchPattern}
