2a1e98b586059e4b879c0eabdc5f2666d454c692
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-01-25 17:49:14 +0100

    fix styled section

--- components/Layout/StyledSection/index.js
+++ components/Layout/StyledSection/index.js
@@ -5,6 +5,8 @@ const StyledSection = styled.section`
   padding-top: 35px;
   padding-bottom: 75px;
   max-width: 650px;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export default StyledSection;
