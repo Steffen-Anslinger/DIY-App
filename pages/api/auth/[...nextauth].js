f4750ecb34bda81f6518ac0e65f6e5de2b8caa66
Maik Berdelmann <maikberdelmann@live.com>
2024-02-02 12:45:04 +0100

    add login component

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "Credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "newfishes",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize() {
+            return {
+              id: 1,
+              name: "New Fishes",
+              email: "new@fishes.com",
+              image:
+                "https://avatars.githubusercontent.com/u/41701911?s=200&v=4",
+            };
+          },
+        })
+      : GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+        }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+export default NextAuth(authOptions);
--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -1,14 +1,17 @@
 import Project from "@/db/models/ProjectSchema";
 import dbConnect from "@/db/connect";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(request, response) {
+  const session = await getServerSession(request, response, authOptions);
   await dbConnect();
 
   if (request.method === "GET") {
     const projects = await Project.find();
     return response.status(200).json(projects);
   }
-  if (request.method === "POST") {
+  if (session && request.method === "POST") {
     try {
       const newProject = request.body;
       await Project.create(newProject);
