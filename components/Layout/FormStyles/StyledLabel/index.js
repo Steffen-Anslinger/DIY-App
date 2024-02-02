738d23cc319a9b58d9e7ff00642bb3b0bada3b30
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-02-02 10:57:19 +0100

    update error messages in edit form

--- components/Layout/FormStyles/StyledLabel/index.js
+++ components/Layout/FormStyles/StyledLabel/index.js
@@ -2,11 +2,13 @@ import color from "../../Colors";
 import styled from "styled-components";
 
 const StyledLabel = styled.label`
-  display: block;
+  display: flex;
+  flex-direction: column;
   margin-bottom: 8px;
   font-size: 16px;
   font-weight: 600;
   color: ${color.grey[950]};
+  width: 100%;
 `;
 
 export default StyledLabel;
