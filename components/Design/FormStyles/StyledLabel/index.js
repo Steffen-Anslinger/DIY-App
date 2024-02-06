ebb2855e25efb8592a7937c6b61866b82a06bfb3
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-02-06 10:45:42 +0100

    update buttons

--- components/Design/FormStyles/StyledLabel/index.js
+++ components/Design/FormStyles/StyledLabel/index.js
@@ -4,12 +4,12 @@ import styled from "styled-components";
 const StyledLabel = styled.label`
   display: flex;
   flex-direction: column;
-  margin-bottom: 15px;
   font-size: 16px;
   font-weight: 600;
   color: ${color.grey[950]};
   width: 100%;
   line-height: 30px;
+  justify-content: center;
 `;
 
 export default StyledLabel;
--- components/EditDetails/index.js
+++ components/EditDetails/index.js
@@ -1,12 +1,40 @@
 import React from "react";
 import ProjectDetails from "../ProjectDetails";
 import EditForm from "../EditForm";
-import StyledLink from "../Design/StyledLink";
 import StyledButton from "../Design/StyledButtons";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { mutate } from "swr";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import styled from "styled-components";
+import color from "@/utils/Colors";
+import Link from "next/link";
+
+const StyledButtons = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 5px;
+`;
+
+const StyledLink = styled(Link)`
+  background-color: ${color.grey[200]};
+  color: ${color.grey[950]};
+  width: fit-content;
+  height: fit-content;
+  padding: 5px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  margin-right: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: ${color.blue[300]};
+  }
+`;
 
 export default function EditDetails({
   isFavourite,
@@ -50,25 +78,47 @@ export default function EditDetails({
             isFavourite={isFavourite}
             favourites={favourites}
           />
-          <StyledLink href="/">Back</StyledLink>
-          {session && (
-            <>
-              <StyledButton
-                type="button"
-                name="orange"
-                onClick={() => setEditMode(true)}
-              >
-                Edit
-              </StyledButton>
-              <StyledButton
-                type="button"
-                name="red"
-                onClick={handleDeleteProject}
-              >
-                Delete
-              </StyledButton>
-            </>
-          )}
+          <StyledButtons>
+            <StyledLink href="/">
+              <Image
+                src={
+                  "/assets/keyboard_backspace_FILL0_wght400_GRAD0_opsz24.svg"
+                }
+                alt="Back Button"
+                width={25}
+                height={25}
+              />
+            </StyledLink>
+            {session && (
+              <>
+                <StyledButton
+                  type="button"
+                  name="icon-blue"
+                  onClick={() => setEditMode(true)}
+                >
+                  <Image
+                    src={"/assets/edit_note_FILL0_wght400_GRAD0_opsz24.svg"}
+                    alt="Edit Button"
+                    width={25}
+                    height={25}
+                  />
+                </StyledButton>
+
+                <StyledButton
+                  type="button"
+                  name="icon-red"
+                  onClick={handleDeleteProject}
+                >
+                  <Image
+                    src={"/assets/delete_FILL0_wght400_GRAD0_opsz24.svg"}
+                    alt="Delete Button"
+                    width={25}
+                    height={25}
+                  />
+                </StyledButton>
+              </>
+            )}
+          </StyledButtons>
         </>
       )}
     </>
