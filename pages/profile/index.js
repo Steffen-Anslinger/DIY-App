9eea0e237b4bda521b3f29e8d7e1af8b2886eff5
Helena Nguyen <handmadeviet.duc@gmail.com>
2024-02-06 12:42:52 +0100

    view profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,7 +1,121 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-export default function ProfilePage() {
+import Link from "next/link";
+import ProjectCard from "@/components/ProjectCard";
+import FavouriteButton from "@/components/FavouriteButton";
+import styled from "styled-components";
+import color from "@/utils/Colors";
+import { React } from "react";
+import Masonry from "@mui/lab/Masonry";
+import { createTheme } from "@mui/material/styles";
+
+const StyledList = styled.ul`
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+`;
+const StyledImage = styled(Image)`
+  width: 100%;
+  height: auto;
+  min-height: 150px;
+  object-fit: cover;
+  border-radius: 5px;
+  filter: brightness(70%);
+`;
+const StyledCardTitle = styled.h2`
+  margin: 0;
+  color: white;
+  width: 100%;
+  height: fit-content;
+  @media (max-width: 100px) {
+    font-size: small;
+  }
+  @media (max-width: 400px) {
+    font-size: medium;
+  }
+`;
+const Container = styled.div`
+  display: flex;
+  align-content: space-between;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  padding: 12px;
+  z-index: 1;
+  gap: 5px;
+  flex-wrap: wrap;
+`;
+const TagButtonLine = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  gap: 5px;
+  flex-wrap: nowrap;
+  height: fit-content;
+`;
+const TagLine = styled.div`
+  display: flex;
+  gap: 5px;
+  flex-wrap: wrap;
+  height: fit-content;
+`;
+const TagDuration = styled.p`
+  background: rgba(255, 255, 255, 0.6);
+  max-width: 100px;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  color: ${color.grey[950]};
+  font-weight: 500;
+  @media (max-width: 200px) {
+    font-size: xx-small;
+  }
+  @media (max-width: 400px) {
+    font-size: small;
+  }
+`;
+const TagDifficulty = styled.p`
+  background: rgba(255, 255, 255, 0.6);
+  max-width: 100px;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  color: ${color.grey[950]};
+  font-weight: 500;
+  @media (max-width: 200px) {
+    font-size: xx-small;
+  }
+  @media (max-width: 400px) {
+    font-size: small;
+  }
+`;
+const customBreakpoints = {
+  values: {
+    xs: 0,
+    sm: 300,
+    md: 600,
+    lg: 960,
+    xl: 1280,
+    custom: 1600,
+  },
+};
+const theme = createTheme({
+  breakpoints: customBreakpoints,
+});
+
+export default function ProfilePage({
+  projects,
+  favourites,
+  onToggleFavourite,
+}) {
   const { data: session } = useSession();
+
   if (!session) {
     return (
       <>
@@ -9,6 +123,10 @@ export default function ProfilePage() {
       </>
     );
   }
+  const myProjects = projects.filter(
+    (project) => project.author === session.user.name
+  );
+
   return (
     <>
       <h1>Profile Page</h1>
@@ -19,6 +137,37 @@ export default function ProfilePage() {
         width={100}
         alt="user-picture"
       />
+      <p>My Projects</p>
+      <StyledList>
+        <Masonry theme={theme} columns={{ xs: 1, sm: 2, md: 3 }} spacing={2}>
+          {myProjects.map((project) => (
+            <ProjectCard key={project._id}>
+              <Link href={`/projects/${project._id}`}>
+                <StyledImage
+                  src={project.cover.url}
+                  width={150}
+                  height={150}
+                  alt={project.title}
+                />
+                <Container>
+                  <TagButtonLine>
+                    <TagLine>
+                      <TagDuration>{project.duration}</TagDuration>
+                      <TagDifficulty>{project.difficulty}</TagDifficulty>
+                    </TagLine>
+                    <FavouriteButton
+                      onToggleFavourite={onToggleFavourite}
+                      id={project._id}
+                      favourites={favourites}
+                    />
+                  </TagButtonLine>
+                  <StyledCardTitle>{project.title}</StyledCardTitle>
+                </Container>
+              </Link>
+            </ProjectCard>
+          ))}
+        </Masonry>
+      </StyledList>
     </>
   );
 }
