74d3b40534f3695f85d0622f67f476d075d20404
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-02-07 11:56:04 +0100

    delete component

--- components/Design/StyledBanner/index.js
+++ components/Design/StyledBanner/index.js
@@ -1,39 +1,14 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import color from "../../../utils/Colors";
 
-const baseBannerStyles = css`
+const InfoBanner = styled.div`
   margin: 50% 10%;
   padding: 16px;
   border-radius: 5px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-`;
-
-const warningBannerStyles = css`
-  color: ${color.red[950]};
-  background-color: ${color.red[50]};
-  border: solid 2px ${color.red[500]};
-`;
-
-const infoBanneStyles = css`
   color: ${color.blue[950]};
   background-color: ${color.blue[50]};
   border: solid 2px ${color.blue[700]};
 `;
 
-const StyledBanner = styled.div`
-  ${baseBannerStyles}
-
-  ${(props) =>
-    props.type === "warning" &&
-    css`
-      ${warningBannerStyles}
-    `}
-
-  ${(props) =>
-    props.type === "information" &&
-    css`
-      ${infoBanneStyles}
-    `}
-`;
-
-export default StyledBanner;
+export default InfoBanner;
--- pages/create.js
+++ pages/create.js
@@ -2,7 +2,7 @@ import ProjectForm from "@/components/ProjectForm";
 import { useSession } from "next-auth/react";
 import LoginButton from "@/components/LoginButton";
 import InfoSVG from "@/components/Design/SVGs/InfoIcon";
-import StyledBanner from "@/components/Design/StyledBanner";
+import InfoBanner from "@/components/Design/StyledBanner";
 
 export default function CreatePage() {
   const { data: session } = useSession();
@@ -12,11 +12,11 @@ export default function CreatePage() {
         <ProjectForm />
       ) : (
         <>
-          <StyledBanner type="information">
+          <InfoBanner>
             <InfoSVG />
             <h2>Please Login to create a new project!</h2>
             <LoginButton />
-          </StyledBanner>
+          </InfoBanner>
         </>
       )}
     </>
